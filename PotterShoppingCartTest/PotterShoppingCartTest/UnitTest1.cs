998a81eb50e97ac70ab279fd47301ea51b1911bb
Ray <faultlesswoman@gmail.com>
2016-10-20 23:05:53 +0800

    add test an Episode_1 and an Episode_2

--- PotterShoppingCartTest/PotterShoppingCartTest/UnitTest1.cs
+++ PotterShoppingCartTest/PotterShoppingCartTest/UnitTest1.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PotterShoppingCartTest
 {
     [TestClass]
     public class UnitTest1
     {
+        //1. 一到五集的哈利波特，每一本都是賣100元
+        //2. 如果你從這個系列買了兩本不同的書，則會有5%的折扣
+        //3. 如果你買了三本不同的書，則會有10%的折扣
+        //4. 如果是四本不同的書，則會有20%的折扣
+        //5. 如果你一次買了整套一到五集，恭喜你將享有25%的折扣
+        //6. 需要留意的是，如果你買了四本書，其中三本不同，第四本則是重複的，
+        //   那麼那三本將享有10%的折扣，但重複的那一本，則仍須100元。
+
+
         [TestMethod]
-        public void Tset_Add_Episode_1_To_Shoppingcart_Price_Should_Be_100()
+        public void Tset_Add_An_Episode_1_To_Shoppingcart_Price_Should_Be_100()
         {
             var shoppingcart = new HarryPotterShoppingCart() { Episode_1_Count = 1 };
             var expected = 100;
@@ -16,15 +24,25 @@ namespace PotterShoppingCartTest
             Assert.AreEqual(expected, actual);
         }
 
-        private decimal GetPrice(HarryPotterShoppingCart shoppingcart)
+        [TestMethod]
+        public void Tset_Add_An_Episode_1_And_An_Episode_2_To_Shoppingcart_Price_Should_Be_190()
         {
-            var result = 0;
-            if (shoppingcart.Episode_1_Count == 1) result = 100;
+            var shoppingcart = new HarryPotterShoppingCart() { Episode_1_Count = 1, Episode_2_Count = 1 };
+            var expected = 190;
+            var actual = GetPrice(shoppingcart);
 
+            Assert.AreEqual(expected, actual);
+        }
+
+        private double GetPrice(HarryPotterShoppingCart shoppingcart)
+        {
+            double result = 0;
+            if (shoppingcart.Episode_1_Count == 1) result = 100;
+            
             return result;
         }
 
-        class HarryPotterShoppingCart
+        private class HarryPotterShoppingCart
         {
             public int Episode_1_Count { get; set; } = 0;
             public int Episode_2_Count { get; set; } = 0;
