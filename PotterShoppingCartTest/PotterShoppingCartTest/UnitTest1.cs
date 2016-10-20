f7e44d3db1b32aa8cad3c010a3f7b871a1474dfa
Ray <faultlesswoman@gmail.com>
2016-10-20 22:13:41 +0800

    add test e01 to shoppingcart

---
+++ PotterShoppingCartTest/PotterShoppingCartTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCartTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Tset_Add_Episode_1_To_Shoppingcart_Price_Should_Be_100()
+        {
+
+            Assert.Fail();
+        }
+    }
+}
