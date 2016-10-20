7e1769276982ae2846b241507eb636992ff34025
Ray <faultlesswoman@gmail.com>
2016-10-20 22:53:05 +0800

    Tset_Add_Episode_1_To_Shoppingcart_Price_Should_Be_100 passed

--- PotterShoppingCartTest/PotterShoppingCartTest/UnitTest1.cs
+++ PotterShoppingCartTest/PotterShoppingCartTest/UnitTest1.cs
@@ -9,8 +9,28 @@ namespace PotterShoppingCartTest
         [TestMethod]
         public void Tset_Add_Episode_1_To_Shoppingcart_Price_Should_Be_100()
         {
+            var shoppingcart = new HarryPotterShoppingCart() { Episode_1_Count = 1 };
+            var expected = 100;
+            var actual = GetPrice(shoppingcart);
 
-            Assert.Fail();
+            Assert.AreEqual(expected, actual);
+        }
+
+        private decimal GetPrice(HarryPotterShoppingCart shoppingcart)
+        {
+            var result = 0;
+            if (shoppingcart.Episode_1_Count == 1) result = 100;
+
+            return result;
+        }
+
+        class HarryPotterShoppingCart
+        {
+            public int Episode_1_Count { get; set; } = 0;
+            public int Episode_2_Count { get; set; } = 0;
+            public int Episode_3_Count { get; set; } = 0;
+            public int Episode_4_Count { get; set; } = 0;
+            public int Episode_5_Count { get; set; } = 0;
         }
     }
 }
